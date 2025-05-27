a401419bd9b6b938e9f6d9c5a976d987444bf0e1
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-05-27 16:44:18 -0300

    feat: adding some fields in user dto, and create Status enum and Role enum

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,31 +1,44 @@
 import {
   IsEmail,
+  IsEnum,
   IsNotEmpty,
   IsOptional,
   IsString,
   IsUrl,
   Matches,
 } from 'class-validator';
+import { UserStatus } from '../enums/userStatus.enum';
+import { UserRole } from '../enums/userRole.enum';
 
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
   name: string;
+
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
   @IsNotEmpty()
   @Matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
     message:
       'Password must be at least 8 characters long and contain at least one letter and one number',
   })
   password: string;
+
   @IsNotEmpty()
   @Matches(/^\+?[0-9\s\-()]{10,20}$/, {
     message: 'Phone number format is invalid',
   })
   phone: string;
+
   @IsOptional()
   @IsUrl()
-  profileImageUrl: string;
+  profileImageUrl?: string;
+
+  @IsEnum(UserStatus)
+  isActive?: number;
+
+  @IsEnum(UserRole)
+  role: UserRole;
 }
