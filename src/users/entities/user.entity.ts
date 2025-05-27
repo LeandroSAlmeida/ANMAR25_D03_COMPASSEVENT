ab29831d4f880f4fd8b42d4768164a9767488de3
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-05-27 16:43:28 -0300

    feat: adding some fields in user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,6 @@
+import { UserRole } from '../enums/userRole.enum';
+import { UserStatus } from '../enums/userStatus.enum';
+
 export class User {
   id: string;
   name: string;
@@ -5,6 +8,9 @@ export class User {
   password: string;
   phone: string;
   profileImageUrl: string;
+  createdAt: string;
+  isActive: UserStatus;
+  role: UserRole;
 
   constructor(partial: Partial<User>) {
     Object.assign(this, partial);
