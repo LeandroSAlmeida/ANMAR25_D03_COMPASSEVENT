fd56e862b49da680246ed0bf956ab47a299dd583
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-05-30 16:21:36 -0300

    feat: create resource subscriptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { UsersModule } from './users/users.module';
 import { UploadController } from './aws/s3.controller';
 import { AwsS3Service } from './aws/s3.service';
 import { EventsModule } from './events/events.module';
+import { SubscriptionModule } from './subscription/subscription.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule,
     EventsModule,
+    SubscriptionModule,
   ],
   controllers: [AppController, UploadController],
   providers: [AppService, AwsS3Service],
