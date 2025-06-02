4468a4c124eedbd84fc9081030004ec2f406852b
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-06-02 16:41:46 -0300

    feat: create method event delete in db

--- src/db/events/event.methods.module.ts
+++ src/db/events/event.methods.module.ts
@@ -4,10 +4,23 @@ import { UserMethodsModule } from '../users/user.methods.module';
 import { UpdateEvent } from './update-event';
 import { FindEventById } from './findById-event';
 import { FindAllEvents } from './findAll-event';
+import { DeleteEvent } from './delete-event';
 
 @Module({
   imports: [UserMethodsModule],
-  providers: [CreateEvent, UpdateEvent, FindEventById, FindAllEvents],
-  exports: [CreateEvent, UpdateEvent, FindEventById, FindAllEvents],
+  providers: [
+    CreateEvent,
+    UpdateEvent,
+    FindEventById,
+    FindAllEvents,
+    DeleteEvent,
+  ],
+  exports: [
+    CreateEvent,
+    UpdateEvent,
+    FindEventById,
+    FindAllEvents,
+    DeleteEvent,
+  ],
 })
 export class EventMethodsModule {}
