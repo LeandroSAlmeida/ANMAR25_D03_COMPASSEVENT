ea856734541f7749ae9514905d89c16eb679d3d8
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-06-02 17:48:35 -0300

    feat: create method subscription create in service

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -1,26 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
-import { UpdateSubscriptionDto } from './dto/update-subscription.dto';
+import { CreateSubscription } from 'src/db/subscription/create-subscription';
+import { Subscription } from './entities/subscription.entity';
 
 @Injectable()
 export class SubscriptionService {
-  create(createSubscriptionDto: CreateSubscriptionDto) {
-    return 'This action adds a new subscription';
-  }
+  constructor(private readonly createSubscription: CreateSubscription) {}
 
-  findAll() {
-    return `This action returns all subscription`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} subscription`;
-  }
-
-  update(id: number, updateSubscriptionDto: UpdateSubscriptionDto) {
-    return `This action updates a #${id} subscription`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} subscription`;
+  async create(dto: CreateSubscriptionDto): Promise<Subscription> {
+    return this.createSubscription.execute(dto);
   }
 }
