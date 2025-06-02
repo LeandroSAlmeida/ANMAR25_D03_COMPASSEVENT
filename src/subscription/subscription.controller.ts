36ae5e998af1f1dcd796343aea5f5d17c874cfea
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-06-02 19:25:02 -0300

    feat: create method listAll subscription in controller

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -1,14 +1,39 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Query } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 import { Subscription } from './entities/subscription.entity';
+import { ListSubscriptionsByUser } from 'src/db/subscription/listUser-subscription';
 
 @Controller('subscription')
 export class SubscriptionController {
-  constructor(private readonly subscriptionService: SubscriptionService) {}
+  constructor(
+    private readonly subscriptionService: SubscriptionService,
+    private readonly listByUserId: ListSubscriptionsByUser,
+  ) {}
 
   @Post()
   async create(@Body() dto: CreateSubscriptionDto): Promise<Subscription> {
     return this.subscriptionService.create(dto);
+  }
+
+  @Get('subscriptions/:id')
+  async listByUser(
+    @Param('id') id: string,
+    @Query('limit') limit = 10,
+    @Query('lastKey') lastKey?: string,
+  ) {
+    const result = await this.listByUserId.execute(id, Number(limit), lastKey);
+
+    return {
+      data: result.data,
+      pagination: {
+        page: result.pagination.page,
+        limit: result.pagination.limit,
+        total: result.pagination.total,
+        totalPages: result.pagination.totalPages,
+      },
+      nextPageToken: result.nextPageToken,
+      hasMore: result.hasMore,
+    };
   }
 }
