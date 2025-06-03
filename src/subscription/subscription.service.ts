69ef8365ba3eabb229fabac04842dcfbf456e401
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2025-06-03 15:36:15 -0300

    feat: create method subscription delete in service

--- src/subscription/subscription.service.ts
+++ src/subscription/subscription.service.ts
@@ -3,12 +3,14 @@ import { CreateSubscriptionDto } from './dto/create-subscription.dto';
 import { CreateSubscription } from 'src/db/subscription/create-subscription';
 import { Subscription } from './entities/subscription.entity';
 import { ListSubscriptionsByUser } from 'src/db/subscription/listUser-subscription';
+import { DeleteSubscription } from 'src/db/subscription/delete-subscription';
 
 @Injectable()
 export class SubscriptionService {
   constructor(
     private readonly createSubscription: CreateSubscription,
     private readonly listSubscriptionsByUser: ListSubscriptionsByUser,
+    private readonly deleteSubscription: DeleteSubscription,
   ) {}
 
   async create(dto: CreateSubscriptionDto): Promise<Subscription> {
@@ -21,5 +23,9 @@ export class SubscriptionService {
     lastKey?: string,
   ): Promise<{ data: Subscription[]; nextPageToken?: string }> {
     return this.listSubscriptionsByUser.execute(userId, limit, lastKey);
+  }
+
+  async softDeleteSubscription(userId: string, eventId: string) {
+    return await this.deleteSubscription.execute(userId, eventId);
   }
 }
